fce3d09f1e6b8f488fc3aecb41a70c693cac6e4e
shahjahan0351 <shahjahan0351@gmail.com>
2022-04-17 12:33:09 +0530

    first commit to git

---
+++ src/test/java/com/example/sampkle/SampkleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampkle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampkleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
